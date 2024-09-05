eae467bbf61fdf9d5694b0c1a583134d63df1535
KatherineFuenzalida <caterina.fdz@gmail.com>
2024-09-05 00:46:25 -0400

    Code explanation added

--- pf/pf.js
+++ pf/pf.js
@@ -1,23 +1,28 @@
-/*Función para guardar nombre de encuesta, preguntas(con mínimo de 8) y alternativas, 
-validando que los datos ingresados sean correctos*/
-
+// Se crea la función que permitete ingresar nombre de la encuesta, número de preguntas y alternativas.
 function crearEncuesta() {
+  // La variable nombre encuesta guardará el nombre de la encuesta que ingrese el usuario por pantalla.
   const nombreEncuesta = prompt("Ingrese el nombre de la encuesta");
+  // La variable numPreguntas guardará el número de preguntas que el usuario ingrese por pantalla.
   let numPreguntas = parseInt(prompt("¿Cuántas preguntas tendrá la encuesta?"));
-
+// Ciclo para validar que encuesta tenga mínimo 8 preguntas. 
   while (isNaN(numPreguntas) || numPreguntas < 8) {
     alert("Debe ingresar al menos 8 preguntas");
     numPreguntas = parseInt(prompt("Reingrese el número de preguntas que tendrá la encuesta?"));
   }
-
+// La varianle numAlternativas guardará el número de alternativas que tendrá cada pregunta.
   let numAlternativas = parseInt(prompt("¿Cuántas alternativas tendrán las preguntas?"));
+  //Ciclo para validar que la variable reciba un valor válido. 
   while (isNaN(numAlternativas)) {
     alert("Debe ingresar un número válido para las alternativas");
     numAlternativas = parseInt(prompt("Reingrese el número de ALTERNATIVAS?"));
   }
+  // Array para almacenar las preguntas y alternativas de la encuesta.
 encuesta = [];
+//Ciclo para recorrer el número de preguntas y alternativas.
   for (let i = 0; i < numPreguntas; i++) {
+    // La variable preguntaTexto guardará texto de preguntas a través del push, y se irá sobreescribiendo.
     const preguntaTexto = prompt("Escribe el texto para la pregunta " + (i + 1) + ":");
+    // Array para almacenar texto de alternativas de la pregunta i.
     const opciones = [];
     for (let j = 0; j < numAlternativas; j++) {
       const opcionTexto = prompt("Escribe el texto para la opción " + (j + 1),"de la pregunta" + (i + 1) + ": ");
@@ -36,46 +41,56 @@ encuesta = [];
 }
 // Función para mostrar encuesta y guardar respuestas del usuario
 function guardarRespuestas(encuesta) { 
+  // Array para guardar respuestas.
   const respuestas = [];
+  // Ciclo para mostrar encuesta. 
   for (let i = 0; i < encuesta.preguntas.length; i++) {
     const pregunta = encuesta.preguntas[i];
     let mensaje = pregunta.pregunta + "\n\n";
     // Listar las alternativas
     for (let j = 0; j < pregunta.opciones.length; j++) {
+      // Se agrega opción numerada a la cadena mensaje, separando cada opción con un salto de línea.
       mensaje += (j+1) + ": " + pregunta.opciones[j] + "\n" ;
     }
 
     let respuestaUsuario
-    // Solicita respuesta  y valida que sea una alternativa válida
+    // Solicita respuesta y valida que sea una alternativa válida. 
     do {
-      respuestaUsuario = parseInt(prompt(mensaje));
+      // Guardo respuesta del usuario para la pregunta i.
+      respuestaUsuario = parseInt(prompt((i+1) + " - " + mensaje));
+      // Muestro respuesta del usuario en consola.
       console.log("respuesta ingresada: " + respuestaUsuario);
+      // Valido que la respuesta ingresa por el usuario exista en el número de laternativas disponibles para elegir.
       if (isNaN(respuestaUsuario) || respuestaUsuario < 1 || respuestaUsuario > pregunta.opciones.length){
-        alert("Debe ingresar un número válido para la respuesta");
+       alert("Debe ingresar un número válido para la respuesta");
       }
     }
+    // Guardo respuestas
         while (isNaN(respuestaUsuario) || respuestaUsuario < 1 || respuestaUsuario > pregunta.opciones.length)
         respuestas.push(pregunta.opciones[respuestaUsuario - 1]);
     }
+    // Devuelvo datos en array respuestas.
   return respuestas;
 }
 
-// Función para mostrar los resultados
+// Función para mostrar los resultados.
 function mostrarResultados(encuesta, respuestas) {
   let resultado = "Resultados de la encuesta \n\n " + encuesta.nombre + ": \n";
+  // Ciclo para recorrer y mostrar cada pregunta y su resultado.
   for (let i = 0; i < encuesta.preguntas.length; i++) {
     resultado += encuesta.preguntas[i].pregunta + "\n" + "- Tu respuesta: " + respuestas[i] + "\n";
   }
-
+// Muestro los resultados por pantalla. 
   alert(resultado);
 }
 
-// Función principal que ejecuta todo el sistema de encuestas
+// Función principal que llama a todas las funciones creadas.
 function sistemaEncuestas() {
+  // Creo encuesta, guardo respuestas y muestro resultados. 
   const encuesta = crearEncuesta();
   const respuestas = guardarRespuestas(encuesta);
   mostrarResultados(encuesta, respuestas);
 }
 
-// Ejecutar el sistema de encuestas
+// Ejecutar el sistema de encuestas.
 sistemaEncuestas();
--- poo/poo.js
+++ poo/poo.js
@@ -1,53 +1,62 @@
+// Creo la clase encuesta con elemntos título y preguntas. 
 class Encuesta {
   constructor(titulo) {
     this.titulo = titulo;
     this.preguntas = [];
   }
-
+  // Creo método para agregarPregunta. 
   agregarPregunta(pregunta) {
     this.preguntas.push(pregunta);
-
   }
 }
-
+// creo la clase Pregunta con un elemento de texto y un arreglo alternativas. 
 class Pregunta {
   constructor(texto) {
     this.texto = texto;
     this.alternativas = [];
   }
+  // Método para guardar alternativas. 
   agregarAlternativa(alternativa) {
     this.alternativas.push(alternativa);
   }
 }
-
+// En esta clase solicito al usuario el título, número de preguntas y alternativas.
 class SistemaEncuestas {
   crearEncuesta() {
     const nombreEncuesta = prompt("Ingrese el título de la encuesta:");
+    // Solicito núm de preguntas y válido que dato sea válido. 
     let numPreguntas = parseInt(prompt("Ingrese el número de preguntas:"));
   while (isNaN(numPreguntas) || numPreguntas < 8) {
     alert("Debe ingresar al menos 8 preguntas");
     numPreguntas = parseInt(prompt("Reingrese el número de preguntas que tendrá la encuesta?"));
   }
-    
+    // Solicito núm de alternativas y válido que dato sea válido. 
   let numOpciones = parseInt(prompt("Ingrese el número de ALTERNATIVAS por pregunta:"));
   while (isNaN(numOpciones)) {
     alert("Debe ingresar un número válido para las alternativas");
     numOpciones = parseInt(prompt("Reingrese el número de ALTERNATIVAS?"));
   }
+  // Creo la instancia de la clase Encuesta con el nombre de la encuesta y las preguntas.
     const encuesta = new Encuesta(nombreEncuesta);
+    // Ciclo para recibir texto de preguntas en i y alternativas en j.
     for (let i = 0; i < numPreguntas; i++) {
       const preguntaTexto = prompt(`Ingrese el texto de la pregunta ${i + 1}:`);
+      // Creo la instancia de la clase Pregunta. 
       const pregunta = new Pregunta(preguntaTexto);
 
       for (let j = 0; j < numOpciones; j++) {
         const alternativaTexto = prompt(`Ingrese el texto de la alternativa ${j + 1} de la pregunta ${i + 1}:`);
+        // Llamo al método para guardar altervativas.
         pregunta.agregarAlternativa(alternativaTexto);
       }
+      // Llamo al método para guardar preguntas.
       encuesta.agregarPregunta(pregunta);
     }
+    // Retorno la encuesta con todas las preguntas y alternativas.
     return encuesta;
   }
-
+/*Creo método para recibir encuestas y validar que dato ingresado se 
+encuentre entre uno y el máx de alternativas definido previamente para las preguntas*/
   capturarRespuestas(encuesta) {
     const arrayrespuestas = [];
     for (let i = 0; i < encuesta.preguntas.length; i++) {
@@ -62,12 +71,15 @@ class SistemaEncuestas {
       //console.log("respuesta ingresada: " + respuesta);
       while (isNaN(respuesta) || respuesta < 1 || respuesta > pregunta.alternativas.length){
         alert("Debe ingresar un número válido para la respuesta");
-        respuesta = parseInt(prompt("Reingrese respuesta?"));
+        respuesta = parseInt(prompt("Reingrese respuesta"));
       }
+      // Guardo la respuesta en array. 
       arrayrespuestas.push(respuesta);
     }
+    // Retorno array con las respuestas.
     return arrayrespuestas; 
   }
+  // Método para mostrar resultados de la encuesta para la pregunta i y su alternativa j.
 mostrarResultados(encuesta, respuestas){
   console.log(`Resultados de la encuesta "${encuesta.titulo}":`);
   for (let i = 0; i < encuesta.preguntas.length; i++) {
@@ -75,12 +87,14 @@ mostrarResultados(encuesta, respuestas){
     console.log(`Respuesta:`, respuestas[i]);
   }
 }
+// Creo las instancias e invoco los métodos.
 ejecutar(){
   const encuesta = sistema.crearEncuesta();
   const respuestas = sistema.capturarRespuestas(encuesta);
   sistema.mostrarResultados(encuesta, respuestas);
 }
 }
-
+// Creo la instancia y llamo a la clases SistemaEncuestas
 const sistema = new SistemaEncuestas();
+// Ejecuto código, los resultados se ven por consola.
 sistema.ejecutar();
